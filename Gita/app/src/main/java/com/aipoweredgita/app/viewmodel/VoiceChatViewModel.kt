12c0236bc6c0954bbf66afd98d2e3b4293e67461
sravanku018 <35286032+sravanku018@users.noreply.github.com>
2026-08-03 11:42:59 +0530

    Restore aiDispatcher and lateinit var aiScope in VoiceChatViewModel

--- Gita/app/src/main/java/com/aipoweredgita/app/viewmodel/VoiceChatViewModel.kt
+++ Gita/app/src/main/java/com/aipoweredgita/app/viewmodel/VoiceChatViewModel.kt
@@ -101,9 +101,8 @@ class VoiceChatViewModel @Inject constructor(
     private var useProxy = false
     private val okHttpClient = GitaApi.sharedOkHttpClient
 
-    private val SUMMARY_THRESHOLD = 20
-
-    private val aiScope = CoroutineScope(viewModelScope.coroutineContext + aiDispatcher + SupervisorJob())
+    private val aiDispatcher = Dispatchers.Default.limitedParallelism(1)
+    private lateinit var aiScope: CoroutineScope
     private val initMutex    = Mutex()
 
     private var startTime     = 0L
